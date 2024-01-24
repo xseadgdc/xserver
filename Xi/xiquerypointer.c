837b1143db566be21cf8a32e951fcad825086761
Enrico Weigelt, metux IT consult <info@metux.net>
2024-01-24 20:04:37 +0100

    (submit/rename-panoramix-sym) rename old symbol PANORAMIX to XINERAMA
    PANORAMIX was the original working title of the extension, before it became
    official standard. Just nobody cared about fixing the symbols to the official
    naming.
    
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- Xi/xiquerypointer.c
+++ Xi/xiquerypointer.c
@@ -48,9 +48,9 @@
 #include "scrnintstr.h"
 #include "xkbsrv.h"
 
-#ifdef PANORAMIX
+#ifdef XINERAMA
 #include "panoramiXsrv.h"
-#endif
+#endif /* XINERAMA */
 
 #include "inpututils.h"
 #include "xiquerypointer.h"
@@ -183,7 +183,7 @@ ProcXIQueryPointer(ClientPtr client)
         rep.win_y = 0;
     }
 
-#ifdef PANORAMIX
+#ifdef XINERAMA
     if (!noPanoramiXExtension) {
         rep.root_x += double_to_fp1616(screenInfo.screens[0]->x);
         rep.root_y += double_to_fp1616(screenInfo.screens[0]->y);
@@ -192,7 +192,7 @@ ProcXIQueryPointer(ClientPtr client)
             rep.win_y += double_to_fp1616(screenInfo.screens[0]->y);
         }
     }
-#endif
+#endif /* XINERAMA */
 
     WriteReplyToClient(client, sizeof(xXIQueryPointerReply), &rep);
     if (buttons)
