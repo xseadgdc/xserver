be817f824748f31a481477c0754015c25630c9d1
Enrico Weigelt, metux IT consult <info@metux.net>
2024-02-22 17:32:26 +0100

    (submit/unexport-ddx-callbacks) os: unexport ddx callbacks
    The DDX callbacks (where core/DIX calls into DDX) aren't supposed to be
    called by drivers directly, so unexport them.
    
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- dix/main.c
+++ dix/main.c
@@ -111,6 +111,7 @@ Equipment Corporation.
 #include "colormapst.h"
 #include "cursorstr.h"
 #include "servermd.h"
+#include "ddx.h"
 #include "dixfont.h"
 #include "extnsionst.h"
 #include "privates.h"
