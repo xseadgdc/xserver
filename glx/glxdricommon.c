pelya/xserver-xsdl

commit 9ef602de46de2beae1d6231dc72a1a783a26122f

os: move xf86PrivsElevated here
Having different types of code all trying to check for elevated privileges
is a bad idea. This implementation is the most thorough one.

Signed-off-by: Nicolai Hähnle <nicolai.haehnle@amd.com>
Reviewed-by: Ben Crocker <bcrocker@redhat.com>
Reviewed-by: Antoine Martin <antoine@nagafix.co.uk>
Tested-by: Ben Crocker <bcrocker@redhat.com>
Reviewed-by: Emil Velikov <emil.velikov@collabora.com>

hw/xfree86/common/xf86Init.c
@@ -238,64 +238,7 @@ xf86PrintBanner(void)
 Bool
 xf86PrivsElevated(void)
 {
-    static Bool privsTested = FALSE;
-    static Bool privsElevated = TRUE;
-
-    if (!privsTested) {
-#if defined(WIN32)
-        privsElevated = FALSE;
-#else
-        if ((getuid() != geteuid()) || (getgid() != getegid())) {
-            privsElevated = TRUE;
-        }
-        else {
-#if defined(HAVE_ISSETUGID)
-            privsElevated = issetugid();
-#elif defined(HAVE_GETRESUID)
-            uid_t ruid, euid, suid;
-            gid_t rgid, egid, sgid;
-
-            if ((getresuid(&ruid, &euid, &suid) == 0) &&
-                (getresgid(&rgid, &egid, &sgid) == 0)) {
-                privsElevated = (euid != suid) || (egid != sgid);
-            }
-            else {
-                printf("Failed getresuid or getresgid");
-                /* Something went wrong, make defensive assumption */
-                privsElevated = TRUE;
-            }
-#else
-            if (getuid() == 0) {
-                /* running as root: uid==euid==0 */
-                privsElevated = FALSE;
-            }
-            else {
-                /*
-                 * If there are saved ID's the process might still be privileged
-                 * even though the above test succeeded. If issetugid() and
-                 * getresgid() aren't available, test this by trying to set
-                 * euid to 0.
-                 */
-                unsigned int oldeuid;
-
-                oldeuid = geteuid();
-
-                if (seteuid(0) != 0) {
-                    privsElevated = FALSE;
-                }
-                else {
-                    if (seteuid(oldeuid) != 0) {
-                        FatalError("Failed to drop privileges.  Exiting\n");
-                    }
-                    privsElevated = TRUE;
-                }
-            }
-#endif
-        }
-#endif
-        privsTested = TRUE;
-    }
-    return privsElevated;
+    return PrivsElevated();
 }
 
 static void

include/os.h
@@ -366,6 +366,9 @@ System(const char *cmdline);
 #define Fclose(a) fclose(a)
 #endif
 
+extern _X_EXPORT Bool
+PrivsElevated(void);
+
 extern _X_EXPORT void
 CheckUserParameters(int argc, char **argv, char **envp);
 extern _X_EXPORT void

os/utils.c
@@ -1719,6 +1719,69 @@ System(const char *cmdline)
 }
 #endif
 
+Bool
+PrivsElevated(void)
+{
+    static Bool privsTested = FALSE;
+    static Bool privsElevated = TRUE;
+
+    if (!privsTested) {
+#if defined(WIN32)
+        privsElevated = FALSE;
+#else
+        if ((getuid() != geteuid()) || (getgid() != getegid())) {
+            privsElevated = TRUE;
+        }
+        else {
+#if defined(HAVE_ISSETUGID)
+            privsElevated = issetugid();
+#elif defined(HAVE_GETRESUID)
+            uid_t ruid, euid, suid;
+            gid_t rgid, egid, sgid;
+
+            if ((getresuid(&ruid, &euid, &suid) == 0) &&
+                (getresgid(&rgid, &egid, &sgid) == 0)) {
+                privsElevated = (euid != suid) || (egid != sgid);
+            }
+            else {
+                printf("Failed getresuid or getresgid");
+                /* Something went wrong, make defensive assumption */
+                privsElevated = TRUE;
+            }
+#else
+            if (getuid() == 0) {
+                /* running as root: uid==euid==0 */
+                privsElevated = FALSE;
+            }
+            else {
+                /*
+                 * If there are saved ID's the process might still be privileged
+                 * even though the above test succeeded. If issetugid() and
+                 * getresgid() aren't available, test this by trying to set
+                 * euid to 0.
+                 */
+                unsigned int oldeuid;
+
+                oldeuid = geteuid();
+
+                if (seteuid(0) != 0) {
+                    privsElevated = FALSE;
+                }
+                else {
+                    if (seteuid(oldeuid) != 0) {
+                        FatalError("Failed to drop privileges.  Exiting\n");
+                    }
+                    privsElevated = TRUE;
+                }
+            }
+#endif
+        }
+#endif
+        privsTested = TRUE;
+    }
+    return privsElevated;
+}
+
 /*
  * CheckUserParameters: check for long command line arguments and long
  * environment variables.  By default, these checks are only done when

xseadgdc/xserver

commit 20b8ce2efb4306792127e5ae8128614acc56e7a9

os: unexport PrivsElevated()
This is an internal function, not used by drivers, which shouldn't have
been exported in the first place.

Fixes: 9ef602de46de2beae1d6231dc72a1a783a26122f
Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

glx/glxdricommon.c
@@ -35,6 +35,9 @@
 #include <GL/gl.h>
 #include <GL/glxtokens.h>
 #include <GL/internal/dri_interface.h>
+
+#include "os/osdep.h"
+
 #include <os.h>
 #include "extinit.h"
 #include "glxserver.h"

hw/xfree86/common/xf86Config.c
@@ -49,6 +49,8 @@
 #include <sys/types.h>
 #include <grp.h>
 
+#include "os/osdep.h"
+
 #include "xf86.h"
 #include "xf86Modes.h"
 #include "xf86Parser.h"

hw/xfree86/common/xf86Init.c
@@ -49,6 +49,7 @@
 #include <X11/Xatom.h>
 
 #include "dix/screenint_priv.h"
+#include "os/osdep.h"
 
 #include "input.h"
 #include "servermd.h"

hw/xfree86/os-support/linux/lnx_init.c
@@ -30,6 +30,8 @@
 #include <X11/X.h>
 #include <X11/Xmd.h>
 
+#include "os/osdep.h"
+
 #include "compiler.h"
 #include "linux.h"
 

include/os.h
@@ -350,9 +350,6 @@ System(const char *cmdline);
 #define Fclose(a) fclose(a)
 #endif
 
-extern _X_EXPORT Bool
-PrivsElevated(void);
-
 extern _X_EXPORT void
 CheckUserParameters(int argc, char **argv, char **envp);
 extern _X_EXPORT void

os/osdep.h
@@ -57,6 +57,7 @@ SOFTWARE.
 
 #include <limits.h>
 #include <stddef.h>
+#include <X11/Xdefs.h>
 #include <X11/Xos.h>
 
 /* If EAGAIN and EWOULDBLOCK are distinct errno values, then we check errno
@@ -143,5 +144,7 @@ static inline void uname(struct utsname *uts) {
     gethostname(uts->nodename, sizeof(uts->nodename));
 }
 #endif /* WIN32 */
+
+Bool PrivsElevated(void);
 
 #endif                          /* _OSDEP_H_ */