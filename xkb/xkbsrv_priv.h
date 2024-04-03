00ae4d0d0b3e7bbec23d30e31fc3fd8354ff8ce3
Enrico Weigelt, metux IT consult <info@metux.net>
2024-04-03 17:25:17 +0200

    xkb: unexport internal variables
    These aren't used by any drivers/modules, and it doesn't seem make much
    sense doing so, thus no need to keep them exported.
    
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- xkb/xkbsrv_priv.h
+++ xkb/xkbsrv_priv.h
@@ -115,4 +115,11 @@ extern DevPrivateKeyRec xkbDevicePrivateKeyRec;
 
 #define XKBDEVICEINFO(dev) ((xkbDeviceInfoPtr)dixLookupPrivate(&(dev)->devPrivates, &xkbDevicePrivateKeyRec))
 
+extern int XkbReqCode;
+extern int XkbEventBase;
+extern int XkbKeyboardErrorCode;
+extern const char *XkbBaseDirectory;
+extern const char *XkbBinDirectory;
+extern CARD32 xkbDebugFlags;
+
 #endif /* _XSERVER_XKBSRV_PRIV_H_ */
