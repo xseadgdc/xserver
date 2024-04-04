aa31ad4433987803ec27ec55fd4b0388e8038462
Enrico Weigelt, metux IT consult <info@metux.net>
2024-04-04 19:43:50 +0200

    xkb: unexport XkbDisableComputedAutoRepeats()
    Not used by any drivers, so no need to keep it exported.
    
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- xkb/xkbsrv_priv.h
+++ xkb/xkbsrv_priv.h
@@ -158,6 +158,7 @@ void XkbProcessKeyboardEvent(DeviceEvent *event, DeviceIntPtr keybd);
 Bool XkbEnableDisableControls(XkbSrvInfoPtr xkbi, unsigned long change,
                               unsigned long newValues, XkbChangesPtr changes,
                               XkbEventCausePtr cause);
+void XkbDisableComputedAutoRepeats(DeviceIntPtr pXDev, unsigned int key);
 
 extern DevPrivateKeyRec xkbDevicePrivateKeyRec;
 
