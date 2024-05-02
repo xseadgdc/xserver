7ff13f102cf28c4ea5fc32dd4d7f26a994d50b79
Enrico Weigelt, metux IT consult <info@metux.net>
2024-05-02 17:52:32 +0200

    (submit/miext-extinit) randr: unexport noRRExtension field
    This field isn't used by any drivers, and also better belongs into
    the corresponding extension instead of OS adaption layer.
    
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- os/utils.c
+++ os/utils.c
@@ -130,9 +130,6 @@ Bool noScreenSaverExtension = FALSE;
 #ifdef MITSHM
 Bool noMITShmExtension = FALSE;
 #endif
-#ifdef RANDR
-Bool noRRExtension = FALSE;
-#endif
 Bool noRenderExtension = FALSE;
 Bool noShapeExtension = FALSE;
 
