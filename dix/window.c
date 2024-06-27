e0f2ea7f9eb9e175f7f23c9f1cd526bdc256ab3c
Enrico Weigelt, metux IT consult <info@metux.net>
2024-06-28 01:18:13 +0200

    (submit/selection.h) dix: rename selection.h to selection_priv.h
    rename it in order to reflects it's private nature (not exported).
    
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- dix/window.c
+++ dix/window.c
@@ -104,6 +104,7 @@ Equipment Corporation.
 #include "dix/exevents_priv.h"
 #include "dix/input_priv.h"
 #include "dix/property_priv.h"
+#include "dix/selection_priv.h"
 #include "os/auth.h"
 #include "os/client_priv.h"
 #include "os/screensaver.h"
@@ -133,7 +134,6 @@ Equipment Corporation.
 #ifdef COMPOSITE
 #include "compint.h"
 #endif
-#include "selection.h"
 #include "inpututils.h"
 #include "privates.h"
 #include "xace.h"
