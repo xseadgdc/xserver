0105b14684a14ecfb2c62f5d6833aa817ac95c26
Enrico Weigelt, metux IT consult <info@metux.net>
2024-06-28 20:17:48 +0200

    (submit/miext-extinit) miext: move over extinit_priv.h from include
    Since most of the extension init logic (and on/off switches for them)
    is driven from miext, this seems the appropriate place for the header.
    
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- Xi/extinit.c
+++ Xi/extinit.c
@@ -63,13 +63,13 @@ SOFTWARE.
 #include "dix/dix_priv.h"
 #include "dix/exevents_priv.h"
 #include "dix/extension_priv.h"
+#include "miext/extinit_priv.h"
 #include "Xext/geext_priv.h"
 
 #include "inputstr.h"
 #include "gcstruct.h"           /* pointer for extnsionst.h */
 #include "extnsionst.h"         /* extension entry   */
 #include "dixevents.h"
-#include "extinit_priv.h"
 #include "exglobals.h"
 #include "swaprep.h"
 #include "privates.h"
