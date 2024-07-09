e7de2379da12929edee87967394b3e0e64d00782
Enrico Weigelt, metux IT consult <info@metux.net>
2024-07-09 10:17:00 +0200

    (submit/cleanup-damageext) damage: minor code formatting cleanups
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- damageext/damageext.c
+++ damageext/damageext.c
@@ -93,13 +93,12 @@ DamageExtNotify(DamageExtPtr pDamageExt, BoxPtr pBoxes, int nBoxes)
 {
     ClientPtr pClient = pDamageExt->pClient;
     DrawablePtr pDrawable = pDamageExt->pDrawable;
-    xDamageNotifyEvent ev;
     int i, x, y, w, h;
 
     damageGetGeometry(pDrawable, &x, &y, &w, &h);
 
     UpdateCurrentTimeIf();
-    ev = (xDamageNotifyEvent) {
+    xDamageNotifyEvent ev = {
         .type = DamageEventBase + XDamageNotify,
         .level = pDamageExt->level,
         .drawable = pDamageExt->drawable,
@@ -181,7 +180,6 @@ ProcDamageQueryVersion(ClientPtr client)
     xDamageQueryVersionReply rep = {
         .type = X_Reply,
         .sequenceNumber = client->sequence,
-        .length = 0
     };
 
     REQUEST(xDamageQueryVersionReq);
