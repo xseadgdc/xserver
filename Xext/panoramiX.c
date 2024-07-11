81168d484c611b0b46bc4ac8bb0e3873868f6763
Enrico Weigelt, metux IT consult <info@metux.net>
2024-07-11 19:54:43 +0200

    Xext: panoramiX: use REQUEST_HEAD_STRUCT and REQUEST_FIELD_* macros
    Use the new macros to make request struct parsing / field swapping
    much easier.
    
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- Xext/panoramiX.c
+++ Xext/panoramiX.c
@@ -32,6 +32,7 @@ Equipment Corporation.
 #include <X11/extensions/panoramiXproto.h>
 
 #include "dix/dix_priv.h"
+#include "dix/request_priv.h"
 
 #include "misc.h"
 #include "cursor.h"
@@ -464,7 +465,8 @@ PanoramiXExtensionInit(void)
     while (panoramiXGeneration != serverGeneration) {
         extEntry = AddExtension(PANORAMIX_PROTOCOL_NAME, 0, 0,
                                 ProcPanoramiXDispatch,
-                                SProcPanoramiXDispatch, PanoramiXResetProc,
+                                ProcPanoramiXDispatch,
+                                PanoramiXResetProc,
                                 StandardMinorOpcode);
         if (!extEntry)
             break;
@@ -900,7 +902,8 @@ PanoramiXResetProc(ExtensionEntry * extEntry)
 int
 ProcPanoramiXQueryVersion(ClientPtr client)
 {
-    /* REQUEST(xPanoramiXQueryVersionReq); */
+    REQUEST_HEAD_STRUCT(xPanoramiXQueryVersionReq);
+
     xPanoramiXQueryVersionReply rep = {
         .type = X_Reply,
         .sequenceNumber = client->sequence,
@@ -909,7 +912,6 @@ ProcPanoramiXQueryVersion(ClientPtr client)
         .minorVersion = SERVER_PANORAMIX_MINOR_VERSION
     };
 
-    REQUEST_SIZE_MATCH(xPanoramiXQueryVersionReq);
     if (client->swapped) {
         swaps(&rep.sequenceNumber);
         swapl(&rep.length);
@@ -923,12 +925,13 @@ ProcPanoramiXQueryVersion(ClientPtr client)
 int
 ProcPanoramiXGetState(ClientPtr client)
 {
-    REQUEST(xPanoramiXGetStateReq);
+    REQUEST_HEAD_STRUCT(xPanoramiXGetStateReq);
+    REQUEST_FIELD_CARD32(window);
+
     WindowPtr pWin;
     xPanoramiXGetStateReply rep;
     int rc;
 
-    REQUEST_SIZE_MATCH(xPanoramiXGetStateReq);
     rc = dixLookupWindow(&pWin, stuff->window, client, DixGetAttrAccess);
     if (rc != Success)
         return rc;
@@ -953,12 +956,13 @@ ProcPanoramiXGetState(ClientPtr client)
 int
 ProcPanoramiXGetScreenCount(ClientPtr client)
 {
-    REQUEST(xPanoramiXGetScreenCountReq);
+    REQUEST_HEAD_STRUCT(xPanoramiXGetScreenCountReq);
+    REQUEST_FIELD_CARD32(window);
+
     WindowPtr pWin;
     xPanoramiXGetScreenCountReply rep;
     int rc;
 
-    REQUEST_SIZE_MATCH(xPanoramiXGetScreenCountReq);
     rc = dixLookupWindow(&pWin, stuff->window, client, DixGetAttrAccess);
     if (rc != Success)
         return rc;
@@ -982,12 +986,13 @@ ProcPanoramiXGetScreenCount(ClientPtr client)
 int
 ProcPanoramiXGetScreenSize(ClientPtr client)
 {
-    REQUEST(xPanoramiXGetScreenSizeReq);
+    REQUEST_HEAD_STRUCT(xPanoramiXGetScreenSizeReq);
+    REQUEST_FIELD_CARD32(window);
+    REQUEST_FIELD_CARD32(screen);
+
     WindowPtr pWin;
     xPanoramiXGetScreenSizeReply rep;
     int rc;
-
-    REQUEST_SIZE_MATCH(xPanoramiXGetScreenSizeReq);
 
     if (stuff->screen >= PanoramiXNumScreens)
         return BadMatch;
@@ -1021,12 +1026,9 @@ ProcPanoramiXGetScreenSize(ClientPtr client)
 int
 ProcXineramaIsActive(ClientPtr client)
 {
-    /* REQUEST(xXineramaIsActiveReq); */
-    xXineramaIsActiveReply rep;
+    REQUEST_HEAD_STRUCT(xXineramaIsActiveReq);
 
-    REQUEST_SIZE_MATCH(xXineramaIsActiveReq);
-
-    rep = (xXineramaIsActiveReply) {
+    xXineramaIsActiveReply rep = {
         .type = X_Reply,
         .sequenceNumber = client->sequence,
         .length = 0,
@@ -1050,7 +1052,8 @@ ProcXineramaIsActive(ClientPtr client)
 int
 ProcXineramaQueryScreens(ClientPtr client)
 {
-    /* REQUEST(xXineramaQueryScreensReq); */
+    REQUEST_HEAD_STRUCT(xXineramaQueryScreensReq);
+
     CARD32 number = (noPanoramiXExtension) ? 0 : PanoramiXNumScreens;
     xXineramaQueryScreensReply rep = {
         .type = X_Reply,
@@ -1058,8 +1061,6 @@ ProcXineramaQueryScreens(ClientPtr client)
         .length = bytes_to_int32(number * sz_XineramaScreenInfo),
         .number = number
     };
-
-    REQUEST_SIZE_MATCH(xXineramaQueryScreensReq);
 
     if (client->swapped) {
         swaps(&rep.sequenceNumber);
