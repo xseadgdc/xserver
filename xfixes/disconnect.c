f05b426cea6fbf4be6f9b4bca93f3e2c86300a5d
Enrico Weigelt, metux IT consult <info@metux.net>
2024-07-12 10:34:41 +0200

    (!1614) xfixes: simplify dispatcher
    The dispatcher functions are much more complex than they're usually are
    (just switch/case statement). Bring them in line with the standard scheme
    used in the Xserver, so further steps become easier.
    
    It's also much cleaner to use the defines from proto headers instead of
    raw numbers.
    
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- xfixes/disconnect.c
+++ xfixes/disconnect.c
@@ -80,7 +80,7 @@ SProcXFixesSetClientDisconnectMode(ClientPtr client)
 
     swapl(&stuff->disconnect_mode);
 
-    return (*ProcXFixesVector[stuff->xfixesReqType]) (client);
+    return ProcXFixesSetClientDisconnectMode(client);
 }
 
 int
@@ -112,7 +112,7 @@ SProcXFixesGetClientDisconnectMode(ClientPtr client)
     REQUEST(xXFixesGetClientDisconnectModeReq);
     REQUEST_SIZE_MATCH(xXFixesGetClientDisconnectModeReq);
 
-    return (*ProcXFixesVector[stuff->xfixesReqType]) (client);
+    return ProcXFixesGetClientDisconnectMode(client);
 }
 
 Bool
