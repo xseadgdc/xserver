9dd3baf88102a66f8233a7e0ff36bfa943a8d61a
Enrico Weigelt, metux IT consult <info@metux.net>
2024-07-12 18:50:09 +0200

    Xext: xvmc: enable swapped requests

--- Xext/xvmc.c
+++ Xext/xvmc.c
@@ -698,13 +698,6 @@ ProcXvMCDispatch(ClientPtr client)
     }
 }
 
-static int _X_COLD
-SProcXvMCDispatch(ClientPtr client)
-{
-    /* We only support local */
-    return BadImplementation;
-}
-
 void
 XvMCExtensionInit(void)
 {
@@ -726,7 +719,7 @@ XvMCExtensionInit(void)
         return;
 
     extEntry = AddExtension(XvMCName, XvMCNumEvents, XvMCNumErrors,
-                            ProcXvMCDispatch, SProcXvMCDispatch,
+                            ProcXvMCDispatch, ProcXvMCDispatch,
                             NULL, StandardMinorOpcode);
 
     if (!extEntry)
