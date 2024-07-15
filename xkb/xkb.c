b00edd7eb941330ce9d66cc74ee06437bbd8b0a5
Enrico Weigelt, metux IT consult <info@metux.net>
2024-07-15 21:59:02 +0200

    (submit/cleanup-xkb) xkb: XkbSendCompatMap(): use stack allocation
    It's simpler and also a little bit faster to put small buffers on stack.
    
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- xkb/xkb.c
+++ xkb/xkb.c
@@ -2802,52 +2802,42 @@ static int
 XkbSendCompatMap(ClientPtr client,
                  XkbCompatMapPtr compat, xkbGetCompatMapReply rep)
 {
-    char *data;
-    int size;
+    char buf[rep.length * 4];
 
     if (rep.length > 0) {
-        data = xallocarray(rep.length, 4);
-        if (data) {
-            register unsigned i, bit;
-            xkbModsWireDesc *grp;
-            XkbSymInterpretPtr sym = &compat->sym_interpret[rep.firstSI];
-            xkbSymInterpretWireDesc *wire = (xkbSymInterpretWireDesc *) data;
+        register unsigned i, bit;
+        xkbModsWireDesc *grp;
+        XkbSymInterpretPtr sym = &compat->sym_interpret[rep.firstSI];
+        xkbSymInterpretWireDesc *wire = (xkbSymInterpretWireDesc *) buf;
 
-            size = rep.length * 4;
-
-            for (i = 0; i < rep.nSI; i++, sym++, wire++) {
-                wire->sym = sym->sym;
-                wire->mods = sym->mods;
-                wire->match = sym->match;
-                wire->virtualMod = sym->virtual_mod;
-                wire->flags = sym->flags;
-                memcpy((char *) &wire->act, (char *) &sym->act,
-                       sz_xkbActionWireDesc);
-                if (client->swapped) {
-                    swapl(&wire->sym);
-                }
-            }
-            if (rep.groups) {
-                grp = (xkbModsWireDesc *) wire;
-                for (i = 0, bit = 1; i < XkbNumKbdGroups; i++, bit <<= 1) {
-                    if (rep.groups & bit) {
-                        grp->mask = compat->groups[i].mask;
-                        grp->realMods = compat->groups[i].real_mods;
-                        grp->virtualMods = compat->groups[i].vmods;
-                        if (client->swapped) {
-                            swaps(&grp->virtualMods);
-                        }
-                        grp++;
-                    }
-                }
-                wire = (xkbSymInterpretWireDesc *) grp;
+        for (i = 0; i < rep.nSI; i++, sym++, wire++) {
+            wire->sym = sym->sym;
+            wire->mods = sym->mods;
+            wire->match = sym->match;
+            wire->virtualMod = sym->virtual_mod;
+            wire->flags = sym->flags;
+            memcpy((char *) &wire->act, (char *) &sym->act,
+                   sz_xkbActionWireDesc);
+            if (client->swapped) {
+                swapl(&wire->sym);
             }
         }
-        else
-            return BadAlloc;
+        if (rep.groups) {
+            grp = (xkbModsWireDesc *) wire;
+            for (i = 0, bit = 1; i < XkbNumKbdGroups; i++, bit <<= 1) {
+                if (rep.groups & bit) {
+                    grp->mask = compat->groups[i].mask;
+                    grp->realMods = compat->groups[i].real_mods;
+                    grp->virtualMods = compat->groups[i].vmods;
+                    if (client->swapped) {
+                        swaps(&grp->virtualMods);
+                    }
+                    grp++;
+                }
+            }
+            wire = (xkbSymInterpretWireDesc *) grp;
+        }
     }
-    else
-        data = NULL;
 
     if (client->swapped) {
         swaps(&rep.sequenceNumber);
@@ -2858,9 +2848,8 @@ XkbSendCompatMap(ClientPtr client,
     }
 
     WriteToClient(client, sizeof(xkbGetCompatMapReply), &rep);
-    if (data) {
-        WriteToClient(client, size, data);
-        free((char *) data);
+    if (rep.length > 0) {
+        WriteToClient(client, sizeof(buf), buf);
     }
     return Success;
 }
