e3756e71db40c1f22e2544dbd00a57e0b872e74b
Enrico Weigelt, metux IT consult <info@metux.net>
2024-07-17 12:34:22 +0200

    (!1601) Xext: xres: ProcXResQueryClients() put temporary int array on stack
    Simplify allocaton by putting the small temporary int array onto stack.
    This also allows further simplifications by upcoming commits.
    
    Note: there's an upper bound by compile time defines (theoretically, can
    be increased by special cmdline args, that virtually nobody ever using).
    
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- Xext/xres.c
+++ Xext/xres.c
@@ -214,12 +214,11 @@ ProcXResQueryVersion(ClientPtr client)
 static int
 ProcXResQueryClients(ClientPtr client)
 {
-    int *current_clients;
     int i, num_clients = 0;
 
     REQUEST_SIZE_MATCH(xXResQueryClientsReq);
 
-    current_clients = xallocarray(currentMaxClients, sizeof(int));
+    int current_clients[currentMaxClients];
 
     for (i = 0; i < currentMaxClients; i++) {
         if (clients[i]) {
@@ -255,8 +254,6 @@ ProcXResQueryClients(ClientPtr client)
             WriteToClient(client, sz_xXResClient, &scratch);
         }
     }
-
-    free(current_clients);
 
     return Success;
 }
