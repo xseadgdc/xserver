9cf63422e2e4abeae940d016806377f2300ad7aa
Enrico Weigelt, metux IT consult <info@metux.net>
2024-07-30 21:02:24 +0200

    xnest: replace XConfigureWindow() calls by xcb_configure_window()
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- hw/xnest/xnest-xcb.h
+++ hw/xnest/xnest-xcb.h
@@ -27,4 +27,14 @@ uint32_t xnestUpstreamGC(GCPtr pGC);
 typedef XSetWindowAttributes XnSetWindowAttr;
 void xnest_encode_window_attr(XnSetWindowAttr attr, uint32_t mask, uint32_t *values);
 
+typedef struct {
+    int x, y;
+    int width, height;
+    int border_width;
+    uint32_t sibling;
+    int stack_mode;
+} XnWindowChanges;
+
+void xnest_configure_window(xcb_connection_t *conn, uint32_t window, uint32_t mask, XnWindowChanges values);
+
 #endif /* __XNEST__XCB_H */
