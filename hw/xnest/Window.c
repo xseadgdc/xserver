8126a7fec36e6df86a9837810b32724147c6d8af
Enrico Weigelt, metux IT consult <info@metux.net>
2024-07-30 17:49:42 +0200

    (!1654) Xnest: use xcb for (un)mapping windows
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- hw/xnest/Window.c
+++ hw/xnest/Window.c
@@ -364,7 +364,7 @@ xnestRealizeWindow(WindowPtr pWin)
 {
     xnestConfigureWindow(pWin, XCB_CONFIG_WINDOW_SIBLING);
     xnestShapeWindow(pWin);
-    XMapWindow(xnestDisplay, xnestWindow(pWin));
+    xcb_map_window(xnestUpstreamInfo.conn, xnestWindow(pWin));
 
     return TRUE;
 }
@@ -372,8 +372,7 @@ xnestRealizeWindow(WindowPtr pWin)
 Bool
 xnestUnrealizeWindow(WindowPtr pWin)
 {
-    XUnmapWindow(xnestDisplay, xnestWindow(pWin));
-
+    xcb_unmap_window(xnestUpstreamInfo.conn, xnestWindow(pWin));
     return TRUE;
 }
 
