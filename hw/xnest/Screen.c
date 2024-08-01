8a524e3369936c0e2e5a53e5ede021b0f3cc6670
Enrico Weigelt, metux IT consult <info@metux.net>
2024-08-01 20:14:37 +0200

    (!1654) Xnest: directly pass ClearArea request to upstream server
    Instead of going through mi machinery, just pass the ClearArea
    request to the upstream window.
    
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- hw/xnest/Screen.c
+++ hw/xnest/Screen.c
@@ -309,6 +309,7 @@ xnestOpenScreen(ScreenPtr pScreen, int argc, char *argv[])
     pScreen->WindowExposures = xnestWindowExposures;
     pScreen->CopyWindow = xnestCopyWindow;
     pScreen->ClipNotify = xnestClipNotify;
+    pScreen->ClearToBackground = xnest_screen_ClearToBackground;
 
     /* Pixmap procedures */
 
