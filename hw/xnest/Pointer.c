4f56b78e284446d75405c893ef9c58438c85116d
Enrico Weigelt, metux IT consult <info@metux.net>
2024-08-05 10:26:08 +0200

    (!1654) Xnest: replace XChangePointerControl() by xcb_change_pointer_control
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- hw/xnest/Pointer.c
+++ hw/xnest/Pointer.c
@@ -25,6 +25,7 @@ is" without express or implied warranty.
 #include "mipointer.h"
 
 #include "Xnest.h"
+#include "xnest-xcb.h"
 
 #include "Display.h"
 #include "Screen.h"
@@ -39,8 +40,12 @@ DeviceIntPtr xnestPointerDevice = NULL;
 void
 xnestChangePointerControl(DeviceIntPtr pDev, PtrCtrl * ctrl)
 {
-    XChangePointerControl(xnestDisplay, TRUE, TRUE,
-                          ctrl->num, ctrl->den, ctrl->threshold);
+    xcb_change_pointer_control(xnestUpstreamInfo.conn,
+                               ctrl->num,
+                               ctrl->den,
+                               ctrl->threshold,
+                               TRUE,
+                               TRUE);
 }
 
 int
