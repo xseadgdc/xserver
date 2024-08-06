dd7f83657d00359024ba4d2a99ee519398867c40
Enrico Weigelt, metux IT consult <info@metux.net>
2024-08-06 15:23:39 +0200

    (!1639) xquartz: drop swapping request length fields
    The request struct's length fields aren't used anymore - we have the
    client->req_len field instead, which also is bigreq-compatible.
    
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- hw/xquartz/xpr/appledri.c
+++ hw/xquartz/xpr/appledri.c
@@ -399,7 +399,6 @@ static int
 SProcAppleDRIQueryVersion(register ClientPtr client)
 {
     REQUEST(xAppleDRIQueryVersionReq);
-    swaps(&stuff->length);
     return ProcAppleDRIQueryVersion(client);
 }
 
@@ -407,7 +406,6 @@ static int
 SProcAppleDRIQueryDirectRenderingCapable(register ClientPtr client)
 {
     REQUEST(xAppleDRIQueryDirectRenderingCapableReq);
-    swaps(&stuff->length);
     REQUEST_SIZE_MATCH(xAppleDRIQueryDirectRenderingCapableReq);
     swapl(&stuff->screen);
     return ProcAppleDRIQueryDirectRenderingCapable(client);
@@ -417,7 +415,6 @@ static int
 SProcAppleDRIAuthConnection(register ClientPtr client)
 {
     REQUEST(xAppleDRIAuthConnectionReq);
-    swaps(&stuff->length);
     REQUEST_SIZE_MATCH(xAppleDRIAuthConnectionReq);
     swapl(&stuff->screen);
     swapl(&stuff->magic);
@@ -428,7 +425,6 @@ static int
 SProcAppleDRICreateSurface(register ClientPtr client)
 {
     REQUEST(xAppleDRICreateSurfaceReq);
-    swaps(&stuff->length);
     REQUEST_SIZE_MATCH(xAppleDRICreateSurfaceReq);
     swapl(&stuff->screen);
     swapl(&stuff->drawable);
@@ -440,7 +436,6 @@ static int
 SProcAppleDRIDestroySurface(register ClientPtr client)
 {
     REQUEST(xAppleDRIDestroySurfaceReq);
-    swaps(&stuff->length);
     REQUEST_SIZE_MATCH(xAppleDRIDestroySurfaceReq);
     swapl(&stuff->screen);
     swapl(&stuff->drawable);
@@ -451,7 +446,6 @@ static int
 SProcAppleDRICreatePixmap(register ClientPtr client)
 {
     REQUEST(xAppleDRICreatePixmapReq);
-    swaps(&stuff->length);
     REQUEST_SIZE_MATCH(xAppleDRICreatePixmapReq);
     swapl(&stuff->screen);
     swapl(&stuff->drawable);
@@ -462,7 +456,6 @@ static int
 SProcAppleDRIDestroyPixmap(register ClientPtr client)
 {
     REQUEST(xAppleDRIDestroyPixmapReq);
-    swaps(&stuff->length);
     REQUEST_SIZE_MATCH(xAppleDRIDestroyPixmapReq);
     swapl(&stuff->drawable);
     return ProcAppleDRIDestroyPixmap(client);
