800f7c09207e1e890d947ac226b21ff8718afdb4
Enrico Weigelt, metux IT consult <info@metux.net>
2024-08-06 15:21:48 +0200

    (!1639) Xi: drop now obsolete swap procs
    Lots of SProc's have become no-ops, just calling the actual Proc's,
    so we can get rid of them entirely.
    
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- Xi/listdev.c
+++ Xi/listdev.c
@@ -72,18 +72,6 @@ SOFTWARE.
 
 /***********************************************************************
  *
- * This procedure lists the input devices available to the server.
- *
- */
-
-int _X_COLD
-SProcXListInputDevices(ClientPtr client)
-{
-    return (ProcXListInputDevices(client));
-}
-
-/***********************************************************************
- *
  * This procedure calculates the size of the information to be returned
  * for an input device.
  *
--- Xi/xiproperty.c
+++ Xi/xiproperty.c
@@ -1016,12 +1016,6 @@ ProcXGetDeviceProperty(ClientPtr client)
 }
 
 int _X_COLD
-SProcXListDeviceProperties(ClientPtr client)
-{
-    return (ProcXListDeviceProperties(client));
-}
-
-int _X_COLD
 SProcXChangeDeviceProperty(ClientPtr client)
 {
     REQUEST(xChangeDevicePropertyReq);
