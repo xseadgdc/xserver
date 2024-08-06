0b7076cb5362f32a231d0645b4f8103990fe8d44
Enrico Weigelt, metux IT consult <info@metux.net>
2024-08-06 15:19:35 +0200

    (!1639) Xext: xf86bigfont: drop swapping request length fields
    The request struct's length fields aren't used anymore - we have the
    client->req_len field instead, which also is bigreq-compatible.
    
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- Xext/xf86bigfont.c
+++ Xext/xf86bigfont.c
@@ -647,8 +647,6 @@ static int _X_COLD
 SProcXF86BigfontQueryVersion(ClientPtr client)
 {
     REQUEST(xXF86BigfontQueryVersionReq);
-
-    swaps(&stuff->length);
     return ProcXF86BigfontQueryVersion(client);
 }
 
@@ -656,8 +654,6 @@ static int _X_COLD
 SProcXF86BigfontQueryFont(ClientPtr client)
 {
     REQUEST(xXF86BigfontQueryFontReq);
-
-    swaps(&stuff->length);
     REQUEST_SIZE_MATCH(xXF86BigfontQueryFontReq);
     swapl(&stuff->id);
     return ProcXF86BigfontQueryFont(client);
