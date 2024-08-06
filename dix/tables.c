a61b5d11efece63a15c842e3d75ef3818d93b3b6
Enrico Weigelt, metux IT consult <info@metux.net>
2024-08-06 15:26:50 +0200

    (!1639) dix: drop swapping request length fields
    The request struct's length fields aren't used anymore - we have the
    client->req_len field instead, which also is bigreq-compatible.
    
    Also dropping now obsolete SProcNoOperation().
    
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- dix/tables.c
+++ dix/tables.c
@@ -451,7 +451,7 @@ int (*SwappedProcVector[256]) (ClientPtr /* client */) = {
     ProcBadRequest,
     ProcBadRequest,                     /* 125 */
     ProcBadRequest,
-    SProcNoOperation,
+    ProcNoOperation,
     ProcBadRequest,
     ProcBadRequest,
     ProcBadRequest,
