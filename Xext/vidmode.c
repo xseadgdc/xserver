7a8499f33825dd0f2e20cad5fa5dd32ab8f1d878
Enrico Weigelt, metux IT consult <info@metux.net>
2024-08-07 13:59:58 +0200

    (submit/fixup-req-len) Xext: vidmode: drop now obsolete swap procs
    Several SProc's have become no-ops, just calling the actual Proc's,
    so we can get rid of them entirely.
    
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- Xext/vidmode.c
+++ Xext/vidmode.c
@@ -1704,12 +1704,6 @@ ProcVidModeDispatch(ClientPtr client)
 }
 
 static int _X_COLD
-SProcVidModeQueryVersion(ClientPtr client)
-{
-    return ProcVidModeQueryVersion(client);
-}
-
-static int _X_COLD
 SProcVidModeGetModeLine(ClientPtr client)
 {
     REQUEST(xXF86VidModeGetModeLineReq);
@@ -2046,7 +2040,7 @@ SProcVidModeDispatch(ClientPtr client)
     REQUEST(xReq);
     switch (stuff->data) {
     case X_XF86VidModeQueryVersion:
-        return SProcVidModeQueryVersion(client);
+        return ProcVidModeQueryVersion(client);
     case X_XF86VidModeGetModeLine:
         return SProcVidModeGetModeLine(client);
     case X_XF86VidModeGetMonitor:
