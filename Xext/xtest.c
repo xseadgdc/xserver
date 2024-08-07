ba089c08e73adb9a6367e1624c3fa67aa2d68d46
Enrico Weigelt, metux IT consult <info@metux.net>
2024-08-07 12:24:26 +0200

    (!1639) Xext: xtest: drop now obsolete swap procs
    Several SProc's have become no-ops, just calling the actual Proc's,
    so we can get rid of them entirely.
    
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- Xext/xtest.c
+++ Xext/xtest.c
@@ -538,12 +538,6 @@ SProcXTestFakeInput(ClientPtr client)
 }
 
 static int _X_COLD
-SProcXTestGrabControl(ClientPtr client)
-{
-    return ProcXTestGrabControl(client);
-}
-
-static int _X_COLD
 SProcXTestDispatch(ClientPtr client)
 {
     REQUEST(xReq);
@@ -555,7 +549,7 @@ SProcXTestDispatch(ClientPtr client)
     case X_XTestFakeInput:
         return SProcXTestFakeInput(client);
     case X_XTestGrabControl:
-        return SProcXTestGrabControl(client);
+        return ProcXTestGrabControl(client);
     default:
         return BadRequest;
     }
