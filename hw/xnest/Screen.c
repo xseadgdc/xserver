fe8c3755852de030974f0f389e4e4bf2c2c56d53
Enrico Weigelt, metux IT consult <info@metux.net>
2024-08-09 17:51:55 +0200

    (!1654) Xnest: use XCB_CW_* defines instead of CW*
    Use XCB's defines instead of Xlib's ones.
    
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- hw/xnest/Screen.c
+++ hw/xnest/Screen.c
@@ -345,7 +345,7 @@ xnestOpenScreen(ScreenPtr pScreen, int argc, char *argv[])
 
     if (xnestDoFullGeneration) {
 
-        valuemask = CWBackPixel | CWEventMask | CWColormap;
+        valuemask = XCB_CW_BACK_PIXEL | XCB_CW_EVENT_MASK | XCB_CW_COLORMAP;
         attributes.background_pixel = xnestWhitePixel;
         attributes.event_mask = xnestEventMask;
         attributes.colormap =
@@ -389,7 +389,7 @@ xnestOpenScreen(ScreenPtr pScreen, int argc, char *argv[])
 
         XMapWindow(xnestDisplay, xnestDefaultWindows[pScreen->myNum]);
 
-        valuemask = CWBackPixmap | CWColormap;
+        valuemask = XCB_CW_BACK_PIXMAP | XCB_CW_COLORMAP;
         attributes.background_pixmap = xnestScreenSaverPixmap;
         attributes.colormap =
             DefaultColormap(xnestDisplay, DefaultScreen(xnestDisplay));
