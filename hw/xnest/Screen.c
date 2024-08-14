1383c0c30fd3fbcb9cfba62575bf8ee75fa7ea1b
Enrico Weigelt, metux IT consult <info@metux.net>
2024-08-14 15:17:44 +0200

    Xnest: replace XGetWindowAttributes() by xcb_get_geometry()
    Use xcb function instead of Xlib, and also spare one additional
    (unused) request.
    
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- hw/xnest/Screen.c
+++ hw/xnest/Screen.c
@@ -148,7 +148,6 @@ xnestOpenScreen(ScreenPtr pScreen, int argc, char *argv[])
     int numVisuals, numDepths;
     int i, j, depthIndex;
     unsigned long valuemask;
-    XWindowAttributes gattributes;
     VisualID defaultVisual;
     int rootDepth;
     miPointerScreenPtr PointPriv;
@@ -264,9 +263,9 @@ xnestOpenScreen(ScreenPtr pScreen, int argc, char *argv[])
     rootDepth = visuals[xnestDefaultVisualIndex].nplanes;
 
     if (xnestParentWindow != 0) {
-        XGetWindowAttributes(xnestDisplay, xnestParentWindow, &gattributes);
-        xnestGeometry.width = gattributes.width;
-        xnestGeometry.height = gattributes.height;
+        xRectangle r = xnest_get_geometry(xnestUpstreamInfo.conn, xnestParentWindow);
+        xnestGeometry.width = r.width;
+        xnestGeometry.height = r.height;
     }
 
     /* myNum */
