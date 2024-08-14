7603a5d6c1b002d77a7d062d7a8f028fb3a7d642
Enrico Weigelt, metux IT consult <info@metux.net>
2024-08-14 15:17:44 +0200

    Xnest: replace XGetWindowAttributes() by xcb_get_geometry()
    Use xcb function instead of Xlib, and also spare one additional
    (unused) request.
    
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- hw/xnest/xcb.c
+++ hw/xnest/xcb.c
@@ -392,3 +392,29 @@ void xnestGetPointerControl(
     *threshold = reply->threshold;
     free(reply);
 }
+
+xRectangle xnestGetGeometry(xcb_connection_t *conn, uint32_t window)
+{
+    xcb_generic_error_t *err = NULL;
+    xcb_get_geometry_reply_t *reply = xcb_get_geometry_reply(
+        xnestUpstreamInfo.conn,
+        xcb_get_geometry(xnestUpstreamInfo.conn, window),
+        &err);
+
+    if (err) {
+        ErrorF("failed getting window attributes for %d: %d\n", window, err->error_code);
+        free(err);
+        return (xRectangle) { 0 };
+    }
+
+    if (!reply) {
+        ErrorF("failed getting window attributes for %d: no reply\n", window);
+        return (xRectangle) { 0 };
+    }
+
+    return (xRectangle) {
+        .x = reply->x,
+        .y = reply->y,
+        .width = reply->width,
+        .height = reply->height };
+}
