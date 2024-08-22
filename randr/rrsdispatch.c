pelya/xserver-xsdl

commit fc708394318ed92c612e2938b335c08c1ffebb28

Add server support for RRGetScreenResourcesCurrent
This depends on randrproto 1.2.99.1

Signed-off-by: Keith Packard <keithp@keithp.com>

randr/rrsdispatch.c
@@ -415,7 +415,8 @@ int (*SProcRandrVector[RRNumberRequests])(ClientPtr) = {
     SProcRRGetCrtcGamma,	/* 23 */
     SProcRRSetCrtcGamma,	/* 24 */
 /* V1.3 additions */
-    SProcRRSetCrtcTransform,	/* 25 */
-    SProcRRGetCrtcTransform,	/* 26 */
+    SProcRRGetScreenResources,	/* 25 GetScreenResourcesCurrent */
+    SProcRRSetCrtcTransform,	/* 26 */
+    SProcRRGetCrtcTransform,	/* 27 */
 };
 

configure.ac
@@ -664,7 +664,7 @@ XEXT_LIB='$(top_builddir)/Xext/libXext.la'
 XEXTXORG_LIB='$(top_builddir)/Xext/libXextbuiltin.la'
 
 dnl Core modules for most extensions, et al.
-REQUIRED_MODULES="[randrproto >= 1.2] [renderproto >= 0.9.3] [fixesproto >= 4.0] [damageproto >= 1.1] xcmiscproto [xextproto >= 7.0.3] [xproto >= 7.0.13] [xtrans >= 1.2.2] bigreqsproto resourceproto fontsproto [inputproto >= 1.9.99.6] [kbproto >= 1.0.3]"
+REQUIRED_MODULES="[randrproto >= 1.2.99.1] [renderproto >= 0.9.3] [fixesproto >= 4.0] [damageproto >= 1.1] xcmiscproto [xextproto >= 7.0.3] [xproto >= 7.0.13] [xtrans >= 1.2.2] bigreqsproto resourceproto fontsproto [inputproto >= 1.9.99.6] [kbproto >= 1.0.3]"
 REQUIRED_LIBS="xfont xau fontenc [pixman-1 >= 0.13.2]"
 
 dnl HAVE_DBUS is true if we actually have the D-Bus library, whereas

randr/randrstr.h
@@ -388,6 +388,9 @@ int
 ProcRRGetScreenResources (ClientPtr client);
 
 int
+ProcRRGetScreenResourcesCurrent (ClientPtr client);
+
+int
 ProcRRSetScreenConfig (ClientPtr client);
 
 int

randr/rrdispatch.c
@@ -212,7 +212,8 @@ int (*ProcRandrVector[RRNumberRequests])(ClientPtr) = {
     ProcRRGetCrtcGamma,		/* 23 */
     ProcRRSetCrtcGamma,		/* 24 */
 /* V1.3 additions */
-    ProcRRSetCrtcTransform,	/* 25 */
-    ProcRRGetCrtcTransform,	/* 26 */
+    ProcRRGetScreenResourcesCurrent, /* 25 */
+    ProcRRSetCrtcTransform,	/* 26 */
+    ProcRRGetCrtcTransform,	/* 27 */
 };
 

randr/rrscreen.c
@@ -314,8 +314,8 @@ ProcRRSetScreenSize (ClientPtr client)
     return Success;
 }
 
-int
-ProcRRGetScreenResources (ClientPtr client)
+static int
+rrGetScreenResources(ClientPtr client, Bool query)
 {
     REQUEST(xRRGetScreenResourcesReq);
     xRRGetScreenResourcesReply  rep;
@@ -339,7 +339,7 @@ ProcRRGetScreenResources (ClientPtr client)
     pScrPriv = rrGetScrPriv(pScreen);
     rep.pad = 0;
     
-    if (pScrPriv)
+    if (query && pScrPriv)
 	if (!RRGetInfo (pScreen))
 	    return BadAlloc;
 
@@ -461,6 +461,18 @@ ProcRRGetScreenResources (ClientPtr client)
 	xfree (extra);
     }
     return client->noClientException;
+}
+
+int
+ProcRRGetScreenResources (ClientPtr client)
+{
+    return rrGetScreenResources(client, TRUE);
+}
+    
+int
+ProcRRGetScreenResourcesCurrent (ClientPtr client)
+{
+    return rrGetScreenResources(client, FALSE);
 }
 
 typedef struct _RR10Data {

xseadgdc/xserver

commit 561ff51c19a8eca3bf5b02aa32c27a3a878021bd

randr: fix wrong call to RRGetScreenResources() in swapped case
ProcRRGetScreenResources() vs. RRGetScreenResourcesCurrent() have different
semantics - this also must be followed in byte-swapped case.

Fixes: fc70839431 - Add server support for RRGetScreenResourcesCurrent
Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

randr/rrsdispatch.c
@@ -116,6 +116,17 @@ SProcRRGetScreenResources(ClientPtr client)
 }
 
 static int _X_COLD
+SProcRRGetScreenResourcesCurrent(ClientPtr client)
+{
+    REQUEST(xRRGetScreenResourcesCurrentReq);
+
+    REQUEST_SIZE_MATCH(xRRGetScreenResourcesCurrentReq);
+    swaps(&stuff->length);
+    swapl(&stuff->window);
+    return (*ProcRandrVector[stuff->randrReqType]) (client);
+}
+
+static int _X_COLD
 SProcRRGetOutputInfo(ClientPtr client)
 {
     REQUEST(xRRGetOutputInfoReq);
@@ -667,7 +678,7 @@ int (*SProcRandrVector[RRNumberRequests]) (ClientPtr) = {
         SProcRRGetCrtcGamma,    /* 23 */
         SProcRRSetCrtcGamma,    /* 24 */
 /* V1.3 additions */
-        SProcRRGetScreenResources,      /* 25 GetScreenResourcesCurrent */
+        SProcRRGetScreenResourcesCurrent,      /* 25 */
         SProcRRSetCrtcTransform,        /* 26 */
         SProcRRGetCrtcTransform,        /* 27 */
         SProcRRGetPanning,      /* 28 */