pelya/xserver-xsdl

commit 479b2be4badab0a67b1f091feb83c1364e27d783

Clear allocated RandR screen private structure.
Use xcalloc instead of xalloc when allocating this structure to ensure
consistent contents at startup.
(cherry picked from commit 16f4c0c1750824f2e5a001cef82a4122a7a2beb0)

randr/randr.c
@@ -230,7 +230,7 @@ Bool RRScreenInit(ScreenPtr pScreen)
 	RRScreenGeneration = serverGeneration;
     }
 
-    pScrPriv = (rrScrPrivPtr) xalloc (sizeof (rrScrPrivRec));
+    pScrPriv = (rrScrPrivPtr) xcalloc (1, sizeof (rrScrPrivRec));
     if (!pScrPriv)
 	return FALSE;
 

xseadgdc/xserver

commit 7d6372ea3b7a2199f3c6aae1dea9d6655135adb7

randr: RRScreenInit(): drop unnecessary zero'ing
The whole struct is already allocated by calloc(), so no need to explicitly
zero-out individual fields.

Fixes: 479b2be4ba - Clear allocated RandR screen private structure
Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

randr/randr.c
@@ -318,7 +318,6 @@ RRScreenInit(ScreenPtr pScreen)
     /*
      * Calling function best set these function vectors
      */
-    pScrPriv->rrGetInfo = 0;
     pScrPriv->maxWidth = pScrPriv->minWidth = pScreen->width;
     pScrPriv->maxHeight = pScrPriv->minHeight = pScreen->height;
 
@@ -326,21 +325,11 @@ RRScreenInit(ScreenPtr pScreen)
     pScrPriv->height = pScreen->height;
     pScrPriv->mmWidth = pScreen->mmWidth;
     pScrPriv->mmHeight = pScreen->mmHeight;
-#if RANDR_12_INTERFACE
-    pScrPriv->rrScreenSetSize = NULL;
-    pScrPriv->rrCrtcSet = NULL;
-    pScrPriv->rrCrtcSetGamma = NULL;
-#endif
 #if RANDR_10_INTERFACE
-    pScrPriv->rrSetConfig = 0;
     pScrPriv->rotations = RR_Rotate_0;
     pScrPriv->reqWidth = pScreen->width;
     pScrPriv->reqHeight = pScreen->height;
-    pScrPriv->nSizes = 0;
-    pScrPriv->pSizes = NULL;
     pScrPriv->rotation = RR_Rotate_0;
-    pScrPriv->rate = 0;
-    pScrPriv->size = 0;
 #endif
 
     /*
@@ -355,10 +344,6 @@ RRScreenInit(ScreenPtr pScreen)
 
     pScreen->ConstrainCursorHarder = RRConstrainCursorHarder;
     pScreen->ReplaceScanoutPixmap = RRReplaceScanoutPixmap;
-    pScrPriv->numOutputs = 0;
-    pScrPriv->outputs = NULL;
-    pScrPriv->numCrtcs = 0;
-    pScrPriv->crtcs = NULL;
 
     xorg_list_init(&pScrPriv->leases);
 