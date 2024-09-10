978afb073533ca2a0e7f1936b852feeaee60986e
Enrico Weigelt, metux IT consult <info@metux.net>
2024-09-10 12:35:30 +0200

    (!1682) os: log: drop now meaningless XLOG_FLUSH option
    Since we're not indirectly writing via FILE anymore, this option has
    become meaningless: it meant flushing out our in-process buffer to
    the kernel, but we're now doing direct write() calls anyways.
    
    xf86 still accepts the "flush" config file flag for backwards compatibility,
    but it hasn't any practical meaning anymore.
    
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- os/log.c
+++ os/log.c
@@ -118,7 +118,6 @@ void (*OsVendorVErrorFProc) (const char *, va_list args) = NULL;
 #endif
 
 static int logFileFd = -1;
-static Bool logFlush = FALSE;
 static Bool logSync = FALSE;
 static int logVerbosity = DEFAULT_LOG_VERBOSITY;
 static int logFileVerbosity = DEFAULT_LOG_FILE_VERBOSITY;
@@ -341,9 +340,6 @@ Bool
 LogSetParameter(LogParameter param, int value)
 {
     switch (param) {
-    case XLOG_FLUSH:
-        logFlush = value ? TRUE : FALSE;
-        return TRUE;
     case XLOG_SYNC:
         logSync = value ? TRUE : FALSE;
         return TRUE;
@@ -604,7 +600,7 @@ LogSWrite(int verb, const char *buf, size_t len, Bool end_line)
         if (inSignalContext && logFileFd >= 0) {
             ret = write(logFileFd, buf, len);
 #ifndef WIN32
-            if (logFlush && logSync)
+            if (logSync)
                 fsync(logFileFd);
 #endif
         }
@@ -620,12 +616,10 @@ LogSWrite(int verb, const char *buf, size_t len, Bool end_line)
             }
             newline = end_line;
             write(logFileFd, buf, len);
-            if (logFlush) {
 #ifndef WIN32
-                if (logSync)
-                    fsync(logFileFd);
+            if (logSync)
+                fsync(logFileFd);
 #endif
-            }
         }
         else if (!inSignalContext && needBuffer) {
             if (len > bufferUnused) {
