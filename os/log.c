ea7623a83012f6d9919e26b17c2d2812f2d68a96
Enrico Weigelt, metux IT consult <info@metux.net>
2024-09-12 17:39:33 +0200

    (!1691) os: log: replace VErrorFSigSafe() by VErrorF()
    Since VErrorF() is now signal safe, we can use this one instead.
    Leaving VErrorFSigSafe() macro for backwards compat with drivers.
    
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- os/log.c
+++ os/log.c
@@ -882,7 +882,7 @@ FatalError(const char *f, ...)
         va_end(apple_args);
     }
 #endif
-    VErrorFSigSafe(f, args);
+    VErrorF(f, args);
     va_end(args);
     ErrorF("\n");
     if (!beenhere)
@@ -913,12 +913,6 @@ ErrorF(const char *f, ...)
     va_start(args, f);
     VErrorF(f, args);
     va_end(args);
-}
-
-void
-VErrorFSigSafe(const char *f, va_list args)
-{
-    LogVMessageVerb(X_ERROR, -1, f, args);
 }
 
 void
