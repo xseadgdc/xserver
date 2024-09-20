42a0b2be64263ba78502f30ea3855f795054e000
Enrico Weigelt, metux IT consult <info@metux.net>
2024-09-20 13:00:14 +0200

    (!1714) dix: add per-screen window destructor hook
    Right now, extension specific window destruction procedures are implemented
    by wrapping the ScreenRec's DestroyWindow() proc pointer: the extensions are
    storing the original pointer in their private data and putting in their own one.
    On each call, their proc restores the original one, calls it, and switches back
    again. When multiple extensions doing so, they're forming a kind of daisy chain.
    (the same is done for lots of other procs)
    
    While that approach is looking nice and elegant on the drawing board, it's
    complicated, dangerous like a chainsaw and makes debugging hard, leading to
    pretty blurred API borders.
    
    This commit introduces a simple approach for letting extension hook into the
    window destruction safely, w/o having to care much about side effects with
    the call chain. Extensions now can simply register their destructor proc
    (and an opaque pointer) and get called back - w/o ever having to mess with
    the ScreenRec's internal structures.
    
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- dix/window.c
+++ dix/window.c
@@ -995,8 +995,6 @@ DisposeWindowOptional(WindowPtr pWin)
 static void
 FreeWindowResources(WindowPtr pWin)
 {
-    ScreenPtr pScreen = pWin->drawable.pScreen;
-
     DeleteWindowFromAnySaveSet(pWin);
     DeleteWindowFromAnySelections(pWin);
     DeleteWindowFromAnyEvents(pWin, TRUE);
@@ -1016,8 +1014,9 @@ FreeWindowResources(WindowPtr pWin)
         dixDestroyPixmap(pWin->background.pixmap, 0);
 
     DeleteAllWindowProperties(pWin);
+
     /* We SHOULD check for an error value here XXX */
-    (*pScreen->DestroyWindow) (pWin);
+    dixScreenRaiseWindowDestroy(pWin);
     DisposeWindowOptional(pWin);
 }
 
