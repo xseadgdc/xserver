2652d550f4c8646abc99c0e3627b1224267ab31a
Enrico Weigelt, metux IT consult <info@metux.net>
2024-09-20 13:00:14 +0200

    (!1714) dix: add per-screen window destructor hook
    Right now, extension specific window destruction procedures are implemented
    by wrapping the ScreenRec's DestroyWindow() proc pointer: the extensions are
    storing the original pointer in their private data and putting in their own one.
    On each call, their proc restores the original one, calls it, and switches back
    again. When multiple extensions doing so, they're forming a kind of daisy chain.
    (the same is done for lots of other procs)
    
    While that approach is looking nice and elegant on the drawing board, it's
    complicated, dangerous like a chainsaw and makes debugging hard, leading to
    pretty blurred API borders.
    
    This commit introduces a simple approach for letting extension hook into the
    window destruction safely, w/o having to care much about side effects with
    the call chain. Extensions now can simply register their destructor proc
    (and an opaque pointer) and get called back - w/o ever having to mess with
    the ScreenRec's internal structures.
    
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- dix/dix_priv.h
+++ dix/dix_priv.h
@@ -256,4 +256,17 @@ extern Bool enableBackingStore;
 /* in generated BuiltInAtoms.c */
 void MakePredeclaredAtoms(void);
 
+/*
+ * @brief call screen's window destructors
+ * @see dixScreenHookWindowDestroy
+ * @param pWin the window thats being destroyed
+ * @result the ScreenRec's DestroyWindow() return value
+ *
+ * Call the pluggable window destructors that extensions might have registered on
+ * the screen, and finally call ScreenRec's DestroyWindow proc.
+ *
+ * Should only be called by DIX itself.
+ */
+int dixScreenRaiseWindowDestroy(WindowPtr pWin);
+
 #endif /* _XSERVER_DIX_PRIV_H */
