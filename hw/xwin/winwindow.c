91a89c472ba23622b8fa7a6e44f9b076e38ea4cf
Enrico Weigelt, metux IT consult <info@metux.net>
2024-09-26 17:50:53 +0200

    (!1705) xwin: drop wrapping on ScreenRec->PositionWindow()
    Instead of complicated wrapping, just call fbPositionWindow directly.
    
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- hw/xwin/winwindow.c
+++ hw/xwin/winwindow.c
@@ -117,9 +117,7 @@ winPositionWindowRootless(WindowPtr pWin, int x, int y)
     winTrace("winPositionWindowRootless (%p)\n", pWin);
 #endif
 
-    WIN_UNWRAP(PositionWindow);
-    fResult = (*pScreen->PositionWindow) (pWin, x, y);
-    WIN_WRAP(PositionWindow, winPositionWindowRootless);
+    fbPositionWindow(pWin, x, y);
 
     winUpdateRgnRootless(pWin);
 
