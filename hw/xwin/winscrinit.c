f30c0a080e91cc53558b9c2ae5ff30966c25f865
Enrico Weigelt, metux IT consult <info@metux.net>
2024-09-26 20:14:43 +0200

    (!1705) xwin: drop winCopyWindowMultiWindow()
    This wrapping function for Screen->CopyWindow() is does nothing more than
    just call the original functions. So no need to keep wrapping it at all.
    
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- hw/xwin/winscrinit.c
+++ hw/xwin/winscrinit.c
@@ -417,7 +417,6 @@ winFinishScreenInitFB(int i, ScreenPtr pScreen, int argc, char **argv)
         WRAP(RestackWindow);
         WRAP(ResizeWindow);
         WRAP(MoveWindow);
-        WRAP(CopyWindow);
         WRAP(SetShape);
         WRAP(ModifyPixmapHeader);
 
@@ -432,7 +431,6 @@ winFinishScreenInitFB(int i, ScreenPtr pScreen, int argc, char **argv)
         pScreen->RestackWindow = winRestackWindowMultiWindow;
         pScreen->ResizeWindow = winResizeWindowMultiWindow;
         pScreen->MoveWindow = winMoveWindowMultiWindow;
-        pScreen->CopyWindow = winCopyWindowMultiWindow;
         pScreen->SetShape = winSetShapeMultiWindow;
 
         if (pScreenInfo->fCompositeWM) {
