9b14c360940f96623bf343e6807d53c6fc10ef6c
Enrico Weigelt, metux IT consult <info@metux.net>
2024-09-27 20:45:20 +0200

    dix: add per-screen pixmap destructor mechanism
    Right now, extension specific pixmap destruction procedures are implemented
    by wrapping the ScreenRec's DestroyPixmap() proc pointer: the extensions are
    storing the original pointer in their private data and putting in their own one.
    On each call, their proc restores the original one, calls it, and switches back
    again. When multiple extensions doing so, they're forming a kind of daisy chain.
    (the same is done for lots of other procs)
    
    While that approach is looking nice and elegant on the drawing board, it's
    complicated, dangerous like a chainsaw and makes debugging hard, leading to
    pretty blurred API borders.
    
    It's even getting worse: the proc also has to do ref counting, and only destroy
    the pixmap if refconter reaching zero - that's all done in the individual screen
    drivers. Therefore, all extensions must check for refcnt == 1, in order to know
    when to really act.
    
    This commit introduces a simple approach for letting extension hook into the
    pixmap destruction safely, w/o having to care much about side effects with
    the call chain. Extensions now can simply register their destructor proc
    (and an opaque pointer) and get called back - w/o ever having to mess with
    the ScreenRec's internal structures.
    
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- dix/dispatch.c
+++ dix/dispatch.c
@@ -1471,6 +1471,8 @@ int
 dixDestroyPixmap(void *value, XID pid)
 {
     PixmapPtr pPixmap = (PixmapPtr) value;
+    if (pPixmap && pPixmap->refcnt == 1)
+        dixScreenRaisePixmapDestroy(pPixmap);
     if (pPixmap && pPixmap->drawable.pScreen && pPixmap->drawable.pScreen->DestroyPixmap)
         return pPixmap->drawable.pScreen->DestroyPixmap(pPixmap);
     return TRUE;
