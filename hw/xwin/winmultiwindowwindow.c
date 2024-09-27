5dc401fa845232fb7b5d189a671cd62ec890820b
Enrico Weigelt, metux IT consult <info@metux.net>
2024-09-27 10:47:42 +0200

    (!1705) xwin: drop wrapping on ScreenRec->ModifyPixmapHeader()
    Instead of complicated wrapping, just call fbModifyPixmapHeader() directly.
    
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- hw/xwin/winmultiwindowwindow.c
+++ hw/xwin/winmultiwindowwindow.c
@@ -1094,7 +1094,6 @@ winModifyPixmapHeaderMultiwindow(PixmapPtr pPixmap,
 {
     int i;
     winPrivPixmapPtr pPixmapPriv = winGetPixmapPriv(pPixmap);
-    Bool fResult;
 
     /* reinitialize everything */
     pPixmap->drawable.depth = depth;
@@ -1145,13 +1144,5 @@ winModifyPixmapHeaderMultiwindow(PixmapPtr pPixmap,
 
     winDebug("winModifyPixmapHeaderMultiwindow: falling back\n");
 
-    {
-        ScreenPtr pScreen = pPixmap->drawable.pScreen;
-        winScreenPriv(pScreen);
-        WIN_UNWRAP(ModifyPixmapHeader);
-        fResult = (*pScreen->ModifyPixmapHeader) (pPixmap, width, height, depth, bitsPerPixel, devKind, pPixData);
-        WIN_WRAP(ModifyPixmapHeader, winModifyPixmapHeaderMultiwindow);
-    }
-
-    return fResult;
+    return fbModifyPixmapHeader(pPixmap, width, height, depth, bitsPerPixel, devKind, pPixData);
 }
