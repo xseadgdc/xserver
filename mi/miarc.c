6fce564b6a15dec4d2ac71ba68854ff7b7924ed7
Enrico Weigelt, metux IT consult <info@metux.net>
2024-09-30 17:56:15 +0200

    (!1711) mi: use dixDestroyPixmap() instead of direct driver call
    Direct calls to ScreenRec->DestroyPixmap() blocks cleaning up the wrapping
    jungle, so use the proper dix function instead.
    
    See: https://gitlab.freedesktop.org/xorg/xserver/-/issues/1754
    
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- mi/miarc.c
+++ mi/miarc.c
@@ -1110,7 +1110,7 @@ miWideArc(DrawablePtr pDraw, GCPtr pGC, int narcs, xArc * parcs)
 
 out:
     if (fTricky) {
-        (*pGCTo->pScreen->DestroyPixmap) ((PixmapPtr) pDrawTo);
+        dixDestroyPixmap((PixmapPtr) pDrawTo, 0);
         FreeScratchGC(pGCTo);
     }
 }
