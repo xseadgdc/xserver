b8d2961d830684cc06841ba5381a9c9a269b5117
Enrico Weigelt, metux IT consult <info@metux.net>
2024-10-15 14:54:43 +0200

    x21

--- glx/glxbyteorder.h
+++ glx/glxbyteorder.h
@@ -31,10 +31,6 @@
 #if !defined(__GLXBYTEORDER_H__)
 #define __GLXBYTEORDER_H__
 
-#ifdef HAVE_DIX_CONFIG_H
-#include <dix-config.h>
-#endif
-
 #include "misc.h"
 
 #endif                          /* !defined(__GLXBYTEORDER_H__) */
