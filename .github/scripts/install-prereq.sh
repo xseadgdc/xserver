616dd8a49aae954a65b53e4dec3585ec39bc0735
Enrico Weigelt, metux IT consult <info@metux.net>
2025-02-04 14:45:43 +0100

    .github: workflow for building Xservers and some drivers
    Add a github workflow that builds the Xservers on Linux and also
    runs build-test of several common drivers against xorg sdk.
    
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

---
+++ .github/scripts/install-prereq.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -e
+
+. .github/scripts/util.sh
+
+mkdir -p $X11_BUILD_DIR
+cd $X11_BUILD_DIR
+
+build_meson   rendercheck       https://gitlab.freedesktop.org/xorg/test/rendercheck     rendercheck-1.6
+build_meson   drm               https://gitlab.freedesktop.org/mesa/drm                  libdrm-2.4.121 \
+    -Domap=enabled
+build_meson   libxcvt           https://gitlab.freedesktop.org/xorg/lib/libxcvt          libxcvt-0.1.0
+build_ac      xorgproto         https://gitlab.freedesktop.org/xorg/proto/xorgproto      xorgproto-2024.1
+build_meson   wayland           https://gitlab.freedesktop.org/wayland/wayland           1.21.0 \
+    -Dtests=false -Ddocumentation=false -Ddtd_validation=false
+build_meson   wayland-protocols https://gitlab.freedesktop.org/wayland/wayland-protocols 1.38
+build_meson   libdecor          https://gitlab.freedesktop.org/libdecor/libdecor         0.1.1 \
+    -D{demo,install_demo}=false
+build_meson   libei             https://gitlab.freedesktop.org/libinput/libei            1.0.0 \
+    -Dtests=disabled -Ddocumentation=[] -Dliboeffis=enabled
+build_ac_xts  xts               https://gitlab.freedesktop.org/xorg/test/xts             master # dbbfa96c036e596346147081cbceda136e7c86c1
+
+clone_source piglit             https://gitlab.freedesktop.org/mesa/piglit               main # 265896c86f90cb72e8f218ba6a3617fca8b9a1e3
+
+echo '[xts]' > piglit/piglit.conf
+echo "path=$X11_BUILD_DIR/xts" >> piglit/piglit.conf
