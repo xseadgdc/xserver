d481c8311a3da1058144dabf4e2bfaa9f84adabb
Enrico Weigelt, metux IT consult <info@metux.net>
2025-02-24 12:15:01 +0100

    render: replace xallocarray() by calloc()
    Only key difference that calloc(), in contrast to rellocarray(),
    is zero-initializing. The overhead is hard to measure on today's
    machines, and it's safer programming practise to always allocate
    zero-initialized, so one can't forget to do it explicitly.
    
    Cocci rule:
    
        @@
        expression COUNT;
        expression LEN;
        @@
        - xallocarray(COUNT,LEN)
        + calloc(COUNT,LEN)
    
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- render/filter.c
+++ render/filter.c
@@ -378,7 +378,7 @@ SetPicturePictFilter(PicturePtr pPicture, PictFilterPtr pFilter,
         return BadMatch;
 
     if (nparams != pPicture->filter_nparams) {
-        xFixed *new_params = xallocarray(nparams, sizeof(xFixed));
+        xFixed *new_params = calloc(nparams, sizeof(xFixed));
 
         if (!new_params && nparams)
             return BadAlloc;
