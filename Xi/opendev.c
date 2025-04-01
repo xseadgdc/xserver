24a8300c85f81461eb1abea91d53e1e25ffa9434
Enrico Weigelt, metux IT consult <info@metux.net>
2025-04-01 17:19:15 +0200

    (1886) XI: canonicalize reply struct declarations and naming
    * name all of them "rep", as throughout most of the Xserver codebase
    * always declare them where initialized
    
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- Xi/opendev.c
+++ Xi/opendev.c
@@ -76,7 +76,6 @@ ProcXOpenDevice(ClientPtr client)
     xInputClassInfo evbase[numInputClasses];
     int j = 0;
     int status = Success;
-    xOpenDeviceReply rep;
     DeviceIntPtr dev;
 
     REQUEST(xOpenDeviceReq);
@@ -127,7 +126,8 @@ ProcXOpenDevice(ClientPtr client)
     }
     evbase[j].class = OtherClass;
     evbase[j++].event_type_base = event_base[OtherClass];
-    rep = (xOpenDeviceReply) {
+
+    xOpenDeviceReply rep = {
         .repType = X_Reply,
         .RepType = X_OpenDevice,
         .sequenceNumber = client->sequence,
