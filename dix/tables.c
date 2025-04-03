dec175d72048f902e3dbe90f603fe9a764efadec
Enrico Weigelt, metux IT consult <info@metux.net>
2025-04-03 17:50:06 +0200

    (!1893) dix: write out X_GetAtomName reply directly
    No need for using a complex callback machinery, if we just move the
    little pieces of byte-swapping directly into the request handler.
    
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- dix/tables.c
+++ dix/tables.c
@@ -730,7 +730,7 @@ ReplySwapPtr ReplySwapVector[256] = {
     ReplyNotSwappd,
     ReplyNotSwappd,                             /* 15 */
     ReplyNotSwappd,
-    (ReplySwapPtr) SGetAtomNameReply,
+    ReplyNotSwappd,
     ReplyNotSwappd,
     ReplyNotSwappd,
     (ReplySwapPtr) SGetPropertyReply,           /* 20 */
