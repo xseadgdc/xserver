3f4b59f5b5348cfcbcea28594b50ef7982b8063e
Enrico Weigelt, metux IT consult <info@metux.net>
2025-04-03 16:13:17 +0200

    dix: write out X_GetKeyboardMapping reply directly
    No need for using a complex callback machinery, if we just move the
    little pieces of byte-swapping directly into the request handler.
    
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- dix/tables.c
+++ dix/tables.c
@@ -813,7 +813,7 @@ ReplySwapPtr ReplySwapVector[256] = {
     (ReplySwapPtr) SGenericReply,               /* SQueryExtensionReply, */
     (ReplySwapPtr) SListExtensionsReply,
     ReplyNotSwappd,                             /* 100 */
-    (ReplySwapPtr) SGetKeyboardMappingReply,
+    ReplyNotSwappd,
     ReplyNotSwappd,
     (ReplySwapPtr) SGetKeyboardControlReply,
     ReplyNotSwappd,
