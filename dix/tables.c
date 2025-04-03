70e47367fccc346be741e083c2753a0c524196a5
Enrico Weigelt, metux IT consult <info@metux.net>
2025-04-03 16:26:23 +0200

    (!1893) dix: write out X_GetKeyboardControl reply directly
    No need for using a complex callback machinery, if we just move the
    little pieces of byte-swapping directly into the request handler.
    
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- dix/tables.c
+++ dix/tables.c
@@ -816,7 +816,7 @@ ReplySwapPtr ReplySwapVector[256] = {
     ReplyNotSwappd,                             /* 100 */
     ReplyNotSwappd,
     ReplyNotSwappd,
-    (ReplySwapPtr) SGetKeyboardControlReply,
+    ReplyNotSwappd,
     ReplyNotSwappd,
     ReplyNotSwappd,                             /* 105 */
     (ReplySwapPtr) SGetPointerControlReply,
