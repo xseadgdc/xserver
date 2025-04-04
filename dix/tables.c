c8b8d31d6aef0999c2f9680d04c50dc2470b78c1
Enrico Weigelt, metux IT consult <info@metux.net>
2025-04-04 15:52:04 +0200

    dix: write out X_QueryColors reply directly
    No need for using a complex callback machinery, if we just move the
    little pieces of byte-swapping directly into the request handler.
    
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- dix/tables.c
+++ dix/tables.c
@@ -803,7 +803,7 @@ ReplySwapPtr ReplySwapVector[256] = {
     ReplyNotSwappd,
     ReplyNotSwappd,
     ReplyNotSwappd,                             /* 90 */
-    (ReplySwapPtr) SQueryColorsReply,
+    ReplyNotSwappd,
     (ReplySwapPtr) SLookupColorReply,
     ReplyNotSwappd,
     ReplyNotSwappd,
