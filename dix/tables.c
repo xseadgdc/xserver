ef35352e2b45abb515413d395432eb2699ee1a21
Enrico Weigelt, metux IT consult <info@metux.net>
2025-04-04 14:42:30 +0200

    dix: write out X_AllocColorPlanes reply directly
    No need for using a complex callback machinery, if we just move the
    little pieces of byte-swapping directly into the request handler.
    
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- dix/tables.c
+++ dix/tables.c
@@ -799,7 +799,7 @@ ReplySwapPtr ReplySwapVector[256] = {
     ReplyNotSwappd,
     ReplyNotSwappd,                             /* 85 */
     ReplyNotSwappd,
-    (ReplySwapPtr) SAllocColorPlanesReply,
+    ReplyNotSwappd,
     ReplyNotSwappd,
     ReplyNotSwappd,
     ReplyNotSwappd,                             /* 90 */
