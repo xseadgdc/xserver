900957760eb2541bad05337cdf3f006680406d3e
Enrico Weigelt, metux IT consult <info@metux.net>
2025-04-04 19:26:55 +0200

    dix: write out X_ListProperties reply directly
    No need for using a complex callback machinery, if we just move the
    little pieces of byte-swapping directly into the request handler.
    
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- dix/tables.c
+++ dix/tables.c
@@ -733,7 +733,7 @@ ReplySwapPtr ReplySwapVector[256] = {
     ReplyNotSwappd,
     ReplyNotSwappd,
     ReplyNotSwappd,                             /* 20 */
-    (ReplySwapPtr) SListPropertiesReply,
+    ReplyNotSwappd,
     ReplyNotSwappd,
     ReplyNotSwappd,
     ReplyNotSwappd,
