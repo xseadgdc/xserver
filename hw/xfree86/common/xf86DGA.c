3c069f55e35dd0c29b7efb70379aaf5ac5fc9217
Enrico Weigelt, metux IT consult <info@metux.net>
2025-04-10 20:04:52 +0200

    (!1909) xfree86: use calloc() instead of malloc()
    Using calloc() instead of malloc() as preventive measure, so there
    never can be any hidden bugs or leaks due uninitialized memory.
    
    The extra cost of using this compiler intrinsic should be practically
    impossible to measure - in many cases a good compiler can even deduce
    if certain areas really don't need to be zero'd (because they're written
    to right after allocation) and create more efficient machine code.
    
    The code pathes in question are pretty cold anyways, so it's probably
    not worth even thinking about potential extra runtime costs.
    
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- hw/xfree86/common/xf86DGA.c
+++ hw/xfree86/common/xf86DGA.c
@@ -146,7 +146,7 @@ DGAInit(ScreenPtr pScreen, DGAFunctionPtr funcs, DGAModePtr modes, int num)
     pScreenPriv = DGA_GET_SCREEN_PRIV(pScreen);
 
     if (!pScreenPriv) {
-        if (!(pScreenPriv = (DGAScreenPtr) malloc(sizeof(DGAScreenRec))))
+        if (!(pScreenPriv = calloc(1, sizeof(DGAScreenRec))))
             return FALSE;
         dixSetPrivate(&pScreen->devPrivates, &DGAScreenKeyRec, pScreenPriv);
         dixScreenHookClose(pScreen, DGACloseScreen);
@@ -400,7 +400,7 @@ xf86SetDGAMode(ScrnInfoPtr pScrn, int num, DGADevicePtr devRet)
     else
         return BadValue;
 
-    if (!(device = (DGADevicePtr) malloc(sizeof(DGADeviceRec))))
+    if (!(device = calloc(1, sizeof(DGADeviceRec))))
         return BadAlloc;
 
     if (!pScreenPriv->current) {
@@ -664,7 +664,7 @@ DGACreateColormap(int index, ClientPtr client, int id, int mode, int alloc)
 
     pMode = &(pScreenPriv->modes[mode - 1]);
 
-    if (!(pVisual = malloc(sizeof(VisualRec))))
+    if (!(pVisual = calloc(1, sizeof(VisualRec))))
         return BadAlloc;
 
     pVisual->vid = FakeClientID(0);
@@ -698,7 +698,7 @@ DGACreateColormap(int index, ClientPtr client, int id, int mode, int alloc)
         pVisual->offsetBlue = BitsClear(pVisual->blueMask);
     }
 
-    if (!(fvlp = malloc(sizeof(FakedVisualList)))) {
+    if (!(fvlp = calloc(1, sizeof(FakedVisualList)))) {
         free(pVisual);
         return BadAlloc;
     }
@@ -1682,7 +1682,7 @@ ProcXDGASetClientVersion(ClientPtr client)
 
     REQUEST_SIZE_MATCH(xXDGASetClientVersionReq);
     if ((pPriv = DGA_GETPRIV(client)) == NULL) {
-        pPriv = malloc(sizeof(DGAPrivRec));
+        pPriv = calloc(1, sizeof(DGAPrivRec));
         /* XXX Need to look into freeing this */
         if (!pPriv)
             return BadAlloc;
