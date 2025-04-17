ac0c94b8e669d6c06afb82dc358aff207cc768a9
Enrico Weigelt, metux IT consult <info@metux.net>
2025-04-17 17:42:59 +0200

    Xres: XResQueryClientResources: enable security filtering
    Pass each client we're considering to report through XaceHookClientAccess(),
    so security extensions have a chance to filter them out.
    
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- Xext/xres.c
+++ Xext/xres.c
@@ -309,7 +309,9 @@ ProcXResQueryClientResources(ClientPtr client)
 
     clientID = CLIENT_ID(stuff->xid);
 
-    if ((clientID >= currentMaxClients) || !clients[clientID]) {
+    if ((clientID >= currentMaxClients) || !clients[clientID] ||
+        (XaceHookClientAccess(client, clients[clientID], DixReadAccess)
+                              != Success)) {
         client->errorValue = stuff->xid;
         return BadValue;
     }
