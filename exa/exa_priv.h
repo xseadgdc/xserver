cbb14a0fb1d1b582aab9015f5c2d1cbca0f2f942
Enrico Weigelt, metux IT consult <info@metux.net>
2025-04-22 20:57:38 +0200

    exa: unexport exaMoveOutPixmap()
    Only used inside EXA code, not used by any drivers, so no need to
    keep it exported any longer.
    
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- exa/exa_priv.h
+++ exa/exa_priv.h
@@ -724,4 +724,6 @@ void
 void
  exaPrepareAccessReg_classic(PixmapPtr pPixmap, int index, RegionPtr pReg);
 
+void exaMoveOutPixmap(PixmapPtr pPixmap);
+
 #endif                          /* EXAPRIV_H */
