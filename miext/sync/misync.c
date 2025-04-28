pelya/xserver-xsdl

commit 469d5bf8b75038631c27edbb0f9cdf7d737fa233

mi: Sync: Don't free managed screen private
misync allocates space for its screen private with
dixRegisterPrivateKey, which means it doesn't have to free it at
CloseScreen time; doing so will, in fact, result in a crash.

Signed-off-by: Daniel Stone <daniel@fooishbar.org>
Reviewed-by: James Jones <jajones@nvidia.com>
Acked-by: Tiago Vignatti <tiago.vignatti@nokia.com>

miext/sync/misync.c
@@ -167,7 +167,6 @@ SyncCloseScreen (int i, ScreenPtr pScreen)
     SyncScreenPrivPtr pScreenPriv = SYNC_SCREEN_PRIV(pScreen);
 
     pScreen->CloseScreen = pScreenPriv->CloseScreen;
-    free(pScreenPriv);
 
     return (*pScreen->CloseScreen) (i, pScreen);
 }

xseadgdc/xserver

commit 4a1e0f808233942eaaf816526b620a8f7632c9af

miext: sync: drop useless CloseScreen wrapping
This wrapped CloseScreen function doesn't do anything actually useful,
so no need to keep it around any longer. It used to do a free() on the
private data (which had been wrong in the first place) but this had
been removed and so the whole function became useless, it had been
forgotten to be swept away.

Fixes: 469d5bf8b75038631c27edbb0f9cdf7d737fa233
Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

miext/sync/misync.c
@@ -149,16 +149,6 @@ miSyncGetScreenFuncs(ScreenPtr pScreen)
     return &pScreenPriv->funcs;
 }
 
-static Bool
-SyncCloseScreen(ScreenPtr pScreen)
-{
-    SyncScreenPrivPtr pScreenPriv = SYNC_SCREEN_PRIV(pScreen);
-
-    pScreen->CloseScreen = pScreenPriv->CloseScreen;
-
-    return (*pScreen->CloseScreen) (pScreen);
-}
-
 Bool
 miSyncSetup(ScreenPtr pScreen)
 {
@@ -179,10 +169,6 @@ miSyncSetup(ScreenPtr pScreen)
 
     if (!pScreenPriv->funcs.CreateFence) {
         pScreenPriv->funcs = miSyncScreenFuncs;
-
-        /* Wrap CloseScreen to clean up */
-        pScreenPriv->CloseScreen = pScreen->CloseScreen;
-        pScreen->CloseScreen = SyncCloseScreen;
     }
 
     return TRUE;

miext/sync/misync_priv.h
@@ -13,9 +13,6 @@ extern DevPrivateKeyRec miSyncScreenPrivateKey;
 typedef struct _syncScreenPriv {
     /* Wrappable sync-specific screen functions */
     SyncScreenFuncsRec funcs;
-
-    /* Wrapped screen functions */
-    CloseScreenProcPtr CloseScreen;
 } SyncScreenPrivRec, *SyncScreenPrivPtr;
 
 #define SYNC_SCREEN_PRIV(pScreen)                               \