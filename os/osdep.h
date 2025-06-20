13595b1801fe8c7373c3f58f3b11732ca8e02245
Enrico Weigelt, metux IT consult <info@metux.net>
2025-06-20 14:44:26 +0200

    xfree86: export OsBlockSignals() but keep it internal
    Needed by the int10 module, so has to be in the symbol table.
    But still not supposed to be used by out-of-tree drivers.
    
    Reported-by: stefan11111 <stefan11111@shitposting.expert>
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- os/osdep.h
+++ os/osdep.h
@@ -173,6 +173,8 @@ void OsInit(void);
 void OsCleanup(Bool);
 void OsVendorFatalError(const char *f, va_list args) _X_ATTRIBUTE_PRINTF(1, 0);
 void OsVendorInit(void);
+
+_X_EXPORT /* needed by the int10 module, but should not be used by OOT drivers */
 void OsBlockSignals(void);
 
 _X_EXPORT /* needed by the int10 module, but should not be used by OOT drivers */
