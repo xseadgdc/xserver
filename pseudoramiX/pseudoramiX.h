c4ca14eff06cdeb9f3a9372571d7387d73a100b2
Mike Gelfand <mikedld@mikedld.com>
2025-06-24 22:50:20 +0100

    pseudoramix: add/fix include guards where missing/broken
    Signed-off-by: Mike Gelfand <mikedld@mikedld.com>

--- pseudoramiX/pseudoramiX.h
+++ pseudoramiX/pseudoramiX.h
@@ -2,7 +2,12 @@
  * Minimal implementation of PanoramiX/Xinerama
  */
 
+#ifndef XSERVER_PSEUDORAMIX_H
+#define XSERVER_PSEUDORAMIX_H
+
 void
 PseudoramiXAddScreen(int x, int y, int w, int h);
 void
 PseudoramiXResetScreens(void);
+
+#endif /* XSERVER_PSEUDORAMIX_H */
