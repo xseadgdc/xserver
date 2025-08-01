dc4ea884b356d7bd3d32b043b7271b72eb25fcc8
Enrico Weigelt, metux IT consult <info@metux.net>
2025-08-01 12:31:06 +0200

    .github: add FreeBSD build
    Add building the Xserver in a FreeBSD 14.3 VM.
    
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

---
+++ .github/scripts/freebsd/install-pkg.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+set -e
+
+echo "--> install extra dependencies"
+pkg install -y \
+    curl \
+    git \
+    libdrm \
+    libepoll-shim \
+    libX11 \
+    libxkbfile \
+    libxshmfence \
+    libXfont2 \
+    libxcvt \
+    libglvnd \
+    libepoxy \
+    mesa-dri \
+    mesa-libs \
+    meson \
+    pixman \
+    pkgconf \
+    xcb-util-image \
+    xcb-util-keysyms \
+    xcb-util-renderutil \
+    xcb-util-wm \
+    xkbcomp \
+    xorgproto
