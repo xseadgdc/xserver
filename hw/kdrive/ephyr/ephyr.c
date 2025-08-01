520ae4fb3f82e01a5d8abbb0a1f9c0d4011c88b5
Enrico Weigelt, metux IT consult <info@metux.net>
2025-08-01 17:31:50 +0200

    kdrive: ephyr: don't use %hhu in error messages
    The win32/mingw libc doesn't support this conversion type.
    
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- hw/kdrive/ephyr/ephyr.c
+++ hw/kdrive/ephyr/ephyr.c
@@ -979,9 +979,9 @@ ephyrProcessErrorEvent(xcb_generic_event_t *xev)
     xcb_generic_error_t *e = (xcb_generic_error_t *)xev;
 
     FatalError("X11 error\n"
-               "Error code: %hhu\n"
+               "Error code: %hu\n"
                "Sequence number: %hu\n"
-               "Major code: %hhu\tMinor code: %hu\n"
+               "Major code: %hu\tMinor code: %hu\n"
                "Error value: %u\n",
                e->error_code,
                e->sequence,
